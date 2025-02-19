c81a89867bd3fc3c446991e128554b9ab89fa1e0
Nicolas Popp <nicolaspopp4@gmail.com>
2025-02-19 09:23:50 +0000

    margin

--- models/int_sales_margin.sql
+++ models/int_sales_margin.sql
@@ -1,28 +1,14 @@
-WITH sales_data AS (
-    SELECT
-        s.orders_id,
-        s.pdt_id,
-        s.quantity,
-        s.revenue,
-        -- Ici, tu devras faire la jointure avec un modèle qui contient `purchase_price`
-        p.purchase_price
-    FROM {{ ref('stg_raw__sales') }} s
-    -- Remplace 'stg_raw__product' par le modèle correct si nécessaire
-    JOIN {{ ref('stg_raw__product') }} p
-        ON s.pdt_id = p.pdt_id
-),
+SELECT
+      products_id,
+      date_date,
+      orders_id,
+      revenue,
+      quantity,
+      purchase_price,
+      ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
+      ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin
+  FROM {{ref("stg_raw__sales")}} s
+  LEFT JOIN {{ref("stg_raw__product")}} p
+      USING (products_id)
 
-calculated_margin AS (
-    SELECT
-        orders_id,
-        pdt_id,
-        quantity,
-        revenue,
-        purchase_price,
-        (quantity * purchase_price) AS purchase_cost,
-        (revenue - (quantity * purchase_price)) AS margin
-    FROM sales_data
-)
-
-SELECT * FROM calculated_margin;
 
